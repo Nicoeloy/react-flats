255ac0e88f050fdbef57103c0e2c2934cccc24df
Nicolas Eloy colasrsca@gmail.com <colasrsca@gmail.com>
2022-01-27 15:01:34 +0100

    flat.jsx render() mtd upd & method of class handleClick()

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
 
-import Flat from './flat';
+import flats from '../../data/flats';
 import FlatList from './flat_list';
 import Marker from './marker';
 
 class App extends Component {
   constructor(props) {
     super(props);
+    this.state {
+      selectedFlat: flats[0],
+      flats
+    };
   }
 
   render() {
     return (
       <div>
         <FlatList
+          flats={this.state.flats}
         />
         <div className="map-container">
         </div>
