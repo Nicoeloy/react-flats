155262c7ff3f25eb35b3a6cc0226f3043c74ed8f
Nicolas Eloy colasrsca@gmail.com <colasrsca@gmail.com>
2022-01-27 13:37:35 +0100

    jsx files components creation

---
+++ src/components/app.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import Flat from './flat';
+import FlatList from './flat_list';
+import Marker from './marker';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return ();
+  }
+}
+
+export default App;
